1e149d0919721d0c3329fce337ba1337af71c48e
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-10-06 17:17:15 -0500

    Created the cards , and fixed the ingredients name, and descriptions

---
+++ src/components/MainCardSin.vue
@@ -0,0 +1,28 @@
+<script setup>
+//importing faker to create random names, pictures, and bios 
+import { faker } from '@faker-js/faker'
+//import { RouterLink } from 'vue-router';
+
+const recipename = faker.food.dish()
+const ingre = faker.food.ingredient()
+const  recipe = faker.food.description(3)
+
+
+</script>
+    
+<template>
+    <!-- {{ fullname }}<br /> -->
+    
+    <!-- <RouterLink :to="{name: 'CardDetails', params: {id: recipename} }"> -->
+    <div class="border border-gray-400 rounded-xl shadow bg-gray-200 h-full">
+        <img :src="faker.image.urlLoremFlickr({width: 640, height: 480, category: 'dish'})" class="rounded-t-xl" />
+        <div class="p-4">
+            <p class="mb-2 text-2xl font-bold">{{ recipename }}</p>
+            <p class="mb-3 font-normal text-purple-400">{{ ingre }}</p>
+
+            <p class="mb-3 font-normal text-purple-400">{{ recipe }}</p>
+        </div>
+    </div>
+    <!-- </RouterLink> -->
+
+</template>
---
+++ src/components/MainCards.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MainCardSin from './MainCardSin.vue';
+</script>
+
+<template>
+    <!-- chosing grid, grid column 5, space between each picture 5, padding 10-->
+<div class="grid grid-cols-5 gap-10 p-10">
+    <MainCardSin v-for="card in 10" :key="card"></MainCardSin>
+  <!-- <MainCardSin v-for="card in 10" :key="card"></MainCardSin>  -->
+</div>
+
+</template>
